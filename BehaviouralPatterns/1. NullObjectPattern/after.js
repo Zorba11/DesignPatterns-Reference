0d65893ce599977ea57b681899aa01aa31ebe2a1
Zorba11 <zorbathegreek111@gmail.com>
2021-10-09 13:48:54 -0600

    null obj done

--- BehaviouralPatterns/1. NullObjectPattern/after.js
+++ BehaviouralPatterns/1. NullObjectPattern/after.js
@@ -9,13 +9,29 @@ class User {
   }
 }
 
+class NullUser {
+  constructor() {
+    this.id = -1
+    this.name = 'Guest'
+  }
+
+  hasAccess() {
+    return false
+  }
+}
+
 const users = [
   new User(1, 'Bob'),
   new User(2, 'John')
 ]
 
 function getUser(id) {
-  return users.find(user => user.id === id)
+  const user = users.find(user => user.id === id)
+  if (user == null) {
+    return new NULLUser()
+  } else {
+    return user
+  }
 }
 
 function printUser(id) {
@@ -26,16 +42,14 @@ function printUser(id) {
     This is problematic since we need to remember to always put this every time we use the users name
     It is also bad because if we want to print `Unknown User` instead, we would need to change every place that we put `Guest` which will most likely be all over the application
   */ 
-  let name = 'Guest'
-  if (user != null && user.name != null) name = user.name
-  console.log('Hello ' + name)
+  console.log('Hello ' + user.name)
 
   /*
     This will throw an error if we don't first check that the user object has this function available and isn't null.
   
     This is a lot of extra code to add in every time we want to check user access, and could cause bugs that are easy to miss if we forget to do the null checks.
   */
-  if (user != null && user.hasAccess != null && user.hasAccess()) {
+  if (user.hasAccess()) {
     console.log('You have access')
   } else {
     console.log('You are not allowed here')
---
+++ BehaviouralPatterns/1. NullObjectPattern/readme.md
@@ -0,0 +1,3 @@
+***Null Object Pattern***
+
+The idea is to have a Null object that will act as a default value for a particular object.
